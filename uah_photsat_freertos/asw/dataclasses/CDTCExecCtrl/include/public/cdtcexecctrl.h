bf1f4d8b36b28e5574176950e418107715b5b936
Adrianperezanton345 <adrian.pereza@edu.uah.es>
2025-06-07 12:53:52 +0200

    commit

--- uah_photsat_freertos/asw/dataclasses/CDTCExecCtrl/include/public/cdtcexecctrl.h
+++ uah_photsat_freertos/asw/dataclasses/CDTCExecCtrl/include/public/cdtcexecctrl.h
@@ -3,7 +3,7 @@
 
 //TODO Add in EDROOM model the enumerate to handle ST[129] TCs
 enum TTCExecCtrl {
-	ExecCtrlPrioTC, ExecCtrlReboot, ExecCtrlHK_FDIRTC, ExecCtrlBKGTC
+	ExecCtrlPrioTC, ExecCtrlReboot, ExecCtrlHK_FDIRTC, ExecCtrlBKGTC, ExecCtrlObsMng
 };
 
 class CDTCExecCtrl{
@@ -42,6 +42,16 @@ class CDTCExecCtrl{
 
 
 	//TODO Add in EDROOM model the guard to handle ST[129] TCs
+
+
+	/**
+	 * \brief Check if is a ObsMng telecommand
+	 *
+	 * \return true if it is a ObsMng telecommand
+	 *
+	 */
+	bool IsObsMng(){return (ExecCtrlObsMng==mExecCtrl);}
+
 
 	/**
 	 * \brief Check if is a Background telecommand
